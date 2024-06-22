0804ccbed8a49e8d9603ae4ffe57ffb501a840ab
Kezza2k7 <kalessels09@aol.com>
2024-06-23 00:38:46 +0100

    changed to error

--- python_scripts/hello_world.py
+++ python_scripts/hello_world.py
@@ -1,4 +1,4 @@
 # `data` is available as builtin and is a dictionary with the input data.
 name = data.get("name", "world")
 # `logger` and `time` are available as builtin without the need of explicit import.
-logger.info("Hello {} at {}".format(name, time.time()))
+logger.error("Hello {} at {}".format(name, time.time()))
